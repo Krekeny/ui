dec1a32cfc06bba4dbb0cd36c0cfa194123eee8f
Michael Ehrich <michael.ehrich@krekeny.com>
2022-08-05 11:05:52 +0200

    chore(storybook): not working next integration

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,28 @@
-module.exports = function(api) {
+// module.exports = function (api) {
+//   api.cache(true);
+//   return {
+//     presets: ["next/babel", ["babel-preset-expo", { jsxRuntime: "automatic" }]],
+//     plugins: [
+//       ["@babel/plugin-proposal-class-properties"],
+//       ["@babel/plugin-proposal-private-methods"],
+//       ["@babel/plugin-proposal-private-property-in-object"],
+//       "react-native-reanimated/plugin",
+//     ],
+//   };
+// };
+
+module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: [
+      ["babel-preset-expo", { jsxRuntime: "automatic" }],
+      "@babel/preset-env",
+    ],
+    plugins: [
+      "@babel/plugin-proposal-class-properties",
+      "@babel/plugin-proposal-private-methods",
+      // "@babel/plugin-proposal-private-property-in-object",
+      "react-native-reanimated/plugin",
+    ],
   };
 };
