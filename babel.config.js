0ddb45661efdadc961b0eb6c8e7dc28164121452
Michael Ehrich <michael.ehrich@krekeny.com>
2022-08-12 01:09:19 +0200

    chore(global): gitignore

--- babel.config.js
+++ babel.config.js
@@ -1,16 +1,3 @@
-// module.exports = function (api) {
-//   api.cache(true);
-//   return {
-//     presets: ["next/babel", ["babel-preset-expo", { jsxRuntime: "automatic" }]],
-//     plugins: [
-//       ["@babel/plugin-proposal-class-properties"],
-//       ["@babel/plugin-proposal-private-methods"],
-//       ["@babel/plugin-proposal-private-property-in-object"],
-//       "react-native-reanimated/plugin",
-//     ],
-//   };
-// };
-
 module.exports = function (api) {
   api.cache(true);
   return {
