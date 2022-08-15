1503f15375275d341d82e159cb02a9e0466e200a
Michael Ehrich <michael.ehrich@krekeny.com>
2022-08-15 18:34:28 +0200

    chore(web): trying to expose styles to the shadow dom

--- components/Cta/Cta.jsx
+++ components/Cta/Cta.jsx
@@ -1,19 +1,27 @@
-import React from "react";
-import { TouchableOpacity, Text, StyleSheet } from "react-native";
+import React from 'react';
+import { TouchableOpacity, Text, StyleSheet } from 'react-native';
+import styled from 'styled-components/native';
 
-export const Cta = ({ onPress, text }) => {
-  return (
-    <TouchableOpacity style={styles.container} onPress={onPress}>
-      <Text style={styles.text}>{text}this is just a test</Text>
-    </TouchableOpacity>
-  );
+export const StyleOptions = {
+	container: {
+		paddingHorizontal: 16,
+		paddingVertical: 8,
+		backgroundColor: 'violet',
+	},
+	text: { color: 'black' },
 };
 
-const styles = StyleSheet.create({
-  container: {
-    paddingHorizontal: 16,
-    paddingVertical: 8,
-    backgroundColor: "violet",
-  },
-  text: { color: "black" },
-});
+const Container = styled.TouchableOpacity`
+	padding: 1em 2em;
+	background: violet;
+`;
+
+const styles = StyleSheet.create(StyleOptions);
+
+export const Cta = ({ onPress, text }) => {
+	return (
+		<Container onPress={onPress}>
+			<Text style={styles.text}>{text}this is just a test</Text>
+		</Container>
+	);
+};
--- web/cta.js
+++ web/cta.js
@@ -1,17 +1,43 @@
-import { Cta } from '../components/Cta/Cta';
+import { Cta, StyleOptions } from '../components/Cta/Cta';
 // import { Logo } from '../components/Logo/Logo';
 // import { Test } from '../components/Test/Test';
 import * as ReactDOM from 'react-dom';
 import React from 'react';
+// import { StyleSheet } from 'react-native';
+// import { StyleSheet } from 'react-native-web';
+// import { createStyleResolver } from 'react-native-web';
+// console.log('createStyleResolver', createStyleResolver);
+import { StyleSheetManager } from 'styled-components';
+
+// TODO webcomponents styling?? styling in storybook inline oder css?
 
 class ctaWeb extends HTMLElement {
 	connectedCallback() {
 		const mountPoint = document.createElement('span');
+		const slot = document.createElement('div');
+
+		slot.id = 'react-app-root';
+
+		mountPoint.appendChild(slot);
+
+		const app = document.createElement('div');
+
+		app.id = 'react-app-slot';
+
+		slot.appendChild(app);
+
 		this.attachShadow({ mode: 'open' }).appendChild(mountPoint);
+
+		const styleSlot = this.shadowRoot.querySelector('#react-app-root');
 
 		const name = this.getAttribute('name');
 		const url = 'https://www.google.com/search?q=' + encodeURIComponent(name);
-		const root = ReactDOM.render(<Cta />, mountPoint);
+		const root = ReactDOM.render(
+			<StyleSheetManager target={styleSlot}>
+				<Cta />
+			</StyleSheetManager>,
+			app
+		);
 		// const root = ReactDOM.render(<Logo />, mountPoint);
 		// const root = ReactDOM.render(<Test />, mountPoint);
 	}
